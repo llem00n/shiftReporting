907c6ab3088124667978e09a6a090a163ab8fc05
YehorPorubaimikh <porubaimikh.yehor@gmail.com>
2020-01-21 10:55:47 +0200

    Page configuration

--- src/app/modules/configuration/configuration.component.ts
+++ src/app/modules/configuration/configuration.component.ts
@@ -6,6 +6,7 @@ import { Store, select } from '@ngrx/store';
 import { State, allPlants, plantState } from 'src/app/app-store/';
 import { PlantActions } from 'src/app/app-store/actions';
 import { map, tap } from 'rxjs/operators';
+import { addPlant } from 'src/app/app-store/plant/plant.actions';
 
 @Component({
   selector: 'app-configuration',
@@ -16,33 +17,26 @@ export class ConfigurationComponent implements OnInit {
 
   showedAddForm: string = null;
 
-  // formConfiguration = [
-  //   { key: 'Plant', type: 'select', label: 'Plant', options: [] },
-  //   { key: 'Department', type: 'input', label: 'Department' }
-  // ]
-  formConfiguration = {
-    Plant: { key: 'Plant', type: 'select', label: 'Plant', options: [] },
-    Department: { key: 'Department', type: 'input', label: 'Department' }
-  }
-
+  formConfiguration = [
+    { key: 'Plant', type: 'select', label: 'Plant', options: [], placeholder: 'Select plant' },
+    { key: 'Department', type: 'input', label: 'Department' }
+  ]
 
 
   form: FormGroup;
+  childForm: FormGroup = new FormGroup({});
   addForm: FormGroup;
-
   configControls = {
     Plant: [
-      <Input>{ key: 'Name', type: 'input', label: 'Name' },
-      <Input>{ key: 'Code', type: 'input', label: 'Code' },
-      <Input>{ key: 'Address', type: 'input', label: 'Address' },
+      <Input>{ key: 'Name', type: 'input', label: 'Name', validators: ['required'] },
+      <Input>{ key: 'Code', type: 'input', label: 'Code', validators: ['required'] },
+      <Input>{ key: 'Address', type: 'input', label: 'Address', validators: ['required'] },
     ],
     Department: [
       <Input>{ key: 'Name', type: 'input', label: 'Name' },
       <Input>{ key: 'Description', type: 'input', label: 'Description' },
-      <Input>{ key: 'qwe', type: 'input' },
     ]
   }
-
 
   constructor(
     private store: Store<State>
@@ -51,24 +45,40 @@ export class ConfigurationComponent implements OnInit {
   ngOnInit() {
     this.store.pipe(
       select(allPlants),
-      tap(plants => this.formConfiguration[0])
-    ).subscribe(console.log);
-
-
+      map(plants => this.formConfiguration[0].options = plants
+        .map(i => {
+          return {
+            value: i.plantId,
+            valueView: `${i.name} (${i.code}, ${i.address})`
+          }
+        })
+      )
+    ).subscribe();
     this.store.dispatch(PlantActions.loadPlants());
     this.form = this.createForm(this.formConfiguration)
   }
-
-  createForm(config: { [key: string]: DynControl }): FormGroup {
+  createForm(config: DynControl[]): FormGroup {
     const group = new FormGroup({})
-    Object.keys(config).map(key => {
-      group.addControl(key, new FormControl(''))
+    config.map(i => {
+      group.addControl(i.key, new FormControl(''))
     })
     return group;
   }
 
-  showAddForm(key: string): void {
-    this.showedAddForm = this.showedAddForm !== key ? key : null;
-
+  childFormCreated(form) {
+    this.childForm = form;
+  }
+  
+  addItem(key) {
+    const payload = this.childForm.value;
+    console.log(key);
+    console.log(payload);
+    switch (key) {
+      case 'Plant':        
+        this.store.dispatch(PlantActions.addPlant({plant: payload}))
+        break;    
+      default:
+        break;
+    } 
   }
 }
--- src/app/modules/configuration/configuration.module.ts
+++ src/app/modules/configuration/configuration.module.ts
@@ -3,15 +3,28 @@ import { CommonModule } from '@angular/common';
 import { ConfigurationComponent } from './configuration.component';
 import { FormModule } from '../form/form.module';
 import { DynamicControlsModule } from '../dynamic-controls/dynamic-controls.module';
+import { MatCardModule } from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
 
+const MaterialModules = [
+  MatCardModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatButtonModule,
+  MatInputModule,
+]
 
 @NgModule({
   declarations: [ConfigurationComponent],
   imports: [
     CommonModule,
     FormModule,
-    DynamicControlsModule
+    DynamicControlsModule,
+    MaterialModules
   ],
   exports: [ConfigurationComponent]
 })
