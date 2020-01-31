2f83dacf705f12c20c6ec32416c8114add1cb569
YehorPorubaimikh <porubaimikh.yehor@gmail.com>
2020-01-31 13:32:50 +0200

    fix empty list

--- src/app/modules/configuration/services/configuration.service.ts
+++ src/app/modules/configuration/services/configuration.service.ts
@@ -10,18 +10,18 @@ export class ConfigurationService {
 
   constructor() { }
 
-  createList(data: Array<Plant | Department>): ListData {
+  createList(data: Array<Plant | Department>, actionButtons?: { key: string, title: string }[]): ListData {
     const list = <ListData>{};
     list.tableData = data;
-    list.head = Object.keys(data[0]).map(i => {
+    list.head = data.length ? Object.keys(data[0]).map(i => {
       return {
         key: i, title: i
       }
-    })
-    list.actionButtons = [
+    }) : [];
+    list.actionButtons = data.length ? actionButtons || [
       { key: 'edit', title: 'Edit' },
       { key: 'dlt', title: 'DLT' }
-    ]
+    ] : null;
     return list;
   }
 
