9585a11137dd6f01927f42cef7bd1ee478ee216d
YehorPorubaimikh <porubaimikh.yehor@gmail.com>
2020-02-12 15:09:56 +0200

    add template component

--- src/app/modules/configuration/configuration.component.ts
+++ src/app/modules/configuration/configuration.component.ts
@@ -3,24 +3,20 @@ import { ConfigPlantsComponent } from './components/config-plants/config-plants.
 import { ConfigDepartmentsComponent } from './components/config-departments/config-departments.component';
 import { ConfigShiftComponent } from './components/config-shift/config-shift.component';
 import { ConfigScheduleComponent } from './components/config-schedule/config-schedule.component';
+import { ConfigTemplateComponent } from './components/config-template/config-template.component';
 
 @Component({
   selector: 'app-configuration',
   templateUrl: './configuration.component.html',
   styleUrls: ['./configuration.component.scss']
 })
-export class ConfigurationComponent implements OnInit {
+export class ConfigurationComponent {
   isShow: { [key: string]: boolean } = {};
-
   config = [
     { key: 'plant', component: ConfigPlantsComponent, title: 'Plants configuration', isShow: false },
     { key: 'department', component: ConfigDepartmentsComponent, title: 'Departments configuration', isShow: false },
     { key: 'shift', component: ConfigShiftComponent, title: 'Shifts configuration', isShow: false },
     { key: 'schedule', component: ConfigScheduleComponent, title: 'Schedule configuration', isShow: false },
+    { key: 'template', component: ConfigTemplateComponent, title: 'Template configuration', isShow: true },
   ];
-
-  constructor() { }
-
-  ngOnInit() {
-  }
 }
