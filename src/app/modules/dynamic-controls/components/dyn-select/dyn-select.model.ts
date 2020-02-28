d037bbc2007e2edb9245b8039b69ef19a9b8ff23
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-02-28 10:03:14 +0200

    DynSelect gettter setter for options

--- src/app/modules/dynamic-controls/components/dyn-select/dyn-select.model.ts
+++ src/app/modules/dynamic-controls/components/dyn-select/dyn-select.model.ts
@@ -2,12 +2,25 @@ import { BaseControl } from '../base/base.model';
 
 export class DynSelect extends BaseControl {
   type = 'select';
-  options: { value: string, viewValue: string }[];
+  private _options: { value: string, viewValue: string }[] = [];
   placeholder?: string;
+  private _settings = [
+    { controlId: 'placeholder', label: 'Placeholder', type: 'text' },
+    { controlId: 'optionsString', label: 'Options', type: 'textarea' },
+  ]
   constructor(opt: {} = {}) {
     super(opt);
+    this.settings = this.settings.concat(this._settings);
     this.controlId = this.controlId || this.createControlId(this.type);
     this.placeholder = opt['placeholder'] || '';
-    this.options = opt['options'] || [];
+  }
+
+  get options() { return this._options }
+  set options(options) { this._options = options }
+  get optionsString() { return this._options.map(i => i.value).join('\n') }
+  set optionsString(options) {
+    this._options = options.split('\n')
+      .filter(i => i !== '')
+      .map(i => { return { value: i, viewValue: i } })
   }
 } 
