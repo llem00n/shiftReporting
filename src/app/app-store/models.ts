fbba4ae3f2a6956020b59c7cc1a2bfe70f4aeb51
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-05 01:19:56 +0200

    models

--- src/app/app-store/models.ts
+++ src/app/app-store/models.ts
@@ -3,3 +3,5 @@ export * from './department/department.model';
 export * from './plant/plant.model';
 export * from './schedule/schedule.model';
 export * from './shift/shift.model';
+export * from './data-entry/data-entry.model';
+export * from './template/template.model';
--- src/app/modules/dynamic-controls/models/index.ts
+++ src/app/modules/dynamic-controls/models/index.ts
@@ -1,5 +1,4 @@
 import { DynInput } from '../components/input/input.model';
-import { Select } from '../components/select/select.model';
 import { BaseControl } from '../components/base/base.model';
 import { DynDatetime } from '../components/dyn-datetime/dyn-datetime.model';
 import { DynTextarea } from '../components/dyn-textarea/dyn-textarea.model';
@@ -8,6 +7,21 @@ import { DynSelect } from '../components/dyn-select/dyn-select.model';
 import { DynNumber } from '../components/dyn-number/dyn-number.model';
 import { DynColor } from '../components/dyn-color/dyn-color.model';
 import { DynCheckbox } from '../components/dyn-checkbox/dyn-checkbox.model';
+import { DynText } from '../components/dyn-text/dyn-text.model'
+
+export {
+  DynDatetime,
+  DynTextarea,
+  DynTime,
+  DynSelect,
+  DynInput,
+  DynNumber,
+  DynColor,
+  DynCheckbox,
+  BaseControl,
+  DynText
+}
+
 
 export type DynControl =
   BaseControl
