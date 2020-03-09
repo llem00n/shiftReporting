bf3a6cb628e76c546d71fbe479ea585117b0033f
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-09 10:37:20 +0200

    dynSelect set options

--- src/app/modules/dynamic-controls/components/dyn-select/dyn-select.model.ts
+++ src/app/modules/dynamic-controls/components/dyn-select/dyn-select.model.ts
@@ -3,7 +3,7 @@ import { BaseControl } from '../base/base.model';
 export class DynSelect extends BaseControl {
   type = 'select';
   placeholder?: string;
-  private _options: { value: string, viewValue: string }[] = [];
+  options: { value: string, viewValue: string }[];
   private _settings = [
     { controlId: 'placeholder', label: 'Placeholder', type: 'text' },
     { controlId: 'optionsString', label: 'Options', type: 'textarea' },
@@ -13,13 +13,14 @@ export class DynSelect extends BaseControl {
     this.settings = this.settings.concat(this._settings);
     this.controlId = this.controlId || this.createControlId(this.type);
     this.placeholder = opt['placeholder'] || '';
+    this.options = opt['options'] || [];
   }
 
-  get options() { return this._options }
-  set options(options) { this._options = options }
-  get optionsString() { return this._options.map(i => i.value).join('\n') }
+  // get options() { return this._options }
+  // set options(options) { this._options = options }
+  get optionsString() { return this.options.map(i => i.value).join('\n') }
   set optionsString(options) {
-    this._options = options.split('\n')
+    this.options = options.split('\n')
       .filter(i => i !== '')
       .map(i => { return { value: i, viewValue: i } })
   }
