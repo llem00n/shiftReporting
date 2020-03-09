e69b85748a5004afc4686b03eb15a4ed2f53e1bd
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-09 10:39:23 +0200

    dataEntry model as class

--- src/app/app-store/data-entry/data-entry.model.ts
+++ src/app/app-store/data-entry/data-entry.model.ts
@@ -1,10 +1,19 @@
 import { Template } from '@models/';
 
 export class DataEntry {
-  dataEntryID:	number;
-  scheduleID:	number;
-  createDate:	string;
-  submitDate:	string;
-  template:	Template;
-  submitUser:	string;
+  dataEntryId?: number = null;
+  scheduleId: number = null;
+  createDate: string = null;
+  submitDate: string = null;
+  template: Template = null;
+  submitUser: string = null;
+
+  constructor(opt: {} = {}) {
+    Object.keys(opt).map(key => {
+      if (Object.keys(this).includes(key)) {
+        this[key] = opt[key]
+      }
+    })
+  }
+
 }
