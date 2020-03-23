c46de2ddc09719eb30491b53d0890499d8ec2026
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-23 22:48:49 +0200

    user form

---
+++ src/app/modules/users/components/user-form/user-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { User, DynText, DynCheckbox } from '@models/*';
+import { DialogService } from 'src/app/modules/dialog/dialog.service';
+
+@Component({
+  selector: 'app-user-form',
+  templateUrl: './user-form.component.html',
+  styleUrls: ['./user-form.component.scss']
+})
+export class UserFormComponent implements OnInit {
+  user: User;
+  title = 'Add plant'
+  saveButton = 'Add'
+  config = [
+    new DynText({ controlId: 'firstName', label: 'First name', validators: { required: true } }),
+    new DynText({ controlId: 'secondName', label: 'Second name', validators: { required: true } }),
+    new DynCheckbox({ controlId: 'isActive', label: 'Active user' }),
+    new DynText({ controlId: 'login', label: 'Login', validators: { required: true } }),
+    new DynText({ controlId: 'password', label: 'Password', validators: { required: true } }),
+    new DynText({ controlId: 'email', label: 'Email' }),
+  ];
+
+  constructor(
+    private dialogService: DialogService,
+  ) { }
+
+  ngOnInit(): void {
+    this.getData()
+  }
+  getData() {
+    this.user = new User(this.dialogService.getData())
+    if (this.user.userId) {
+      this.title = 'Edit plant';
+      this.saveButton = 'Save'
+    };
+    console.log(this.user);    
+  }
+  close() {
+    this.dialogService.dismiss()
+  }
+  getForm(e){
+    console.log(e);
+    
+  }
+  save(){
+    console.log('save');
+    
+  }
+}
