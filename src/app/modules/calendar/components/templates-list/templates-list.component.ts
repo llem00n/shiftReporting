515871d86ee506aa3681c2af5cfa6c9d93186575
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-29 23:47:28 +0300

    template-list

--- src/app/modules/calendar/components/templates-list/templates-list.component.ts
+++ src/app/modules/calendar/components/templates-list/templates-list.component.ts
@@ -3,6 +3,10 @@ import { State, DataEntry } from '@models/*';
 import { Store, select } from '@ngrx/store';
 import { Template } from '@models/';
 import { allTemplates, dataEntriesOnDate } from 'src/app/app-store';
+import { Router } from '@angular/router';
+import { tap, mergeMap, map } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { DateService } from 'src/app/services/date.service';
 
 @Component({
   selector: 'app-templates-list',
@@ -13,38 +17,100 @@ export class TemplatesListComponent implements OnInit {
   @Input() day;
   @Input() shift;
 
-  templates: Template[];
+  // svgSrc = "check-circle-outline"
+  svgIcon = {
+    lock: { name: "lock", style: { fill: 'grey', width: '1rem' } },
+    submited: { name: "check-circle-outline", style: { fill: 'green', width: '1rem' } },
+    missed: { name: "checkbox-blank-circle", style: { fill: 'red', width: '1rem' } },
+    open: { name: "checkbox-blank-circle-outline", style: { fill: 'red', width: '1rem' } },
+  }
+
+  templates;
   dataEntries: DataEntry[];
+
+  data$: Subscription
+  dataEntries$: Subscription
+
   constructor(
-    private store: Store<State>
+    private store: Store<State>,
+    private router: Router,
+    private dateService: DateService,
   ) { }
 
   ngOnInit(): void {
-    this.getTemplates();
-    this.getDataEntries();
-    console.log(this.day);
-    console.log(this.shift);
-    
-    
+    this.getData();
+  }
+  ngOnDestroy(): void {
+    this.data$.unsubscribe();
+    // this.dataEntries$.unsubscribe();
+  }
+  getData() {
+    this.data$ = this.store.pipe(
+      select(dataEntriesOnDate),
+      mergeMap(dataEntries => this.store.select(allTemplates).pipe(
+        map(templates => {
+          return {
+            templates,
+            dataEntries
+          }
+        })
+      )),
+      tap(data => this.createData(data)),
+    ).subscribe()
   }
 
-  getTemplates() {
-    this.store.pipe(
-      select(allTemplates)
-    ).subscribe(list => {
-      this.templates = list;
+  createData(data: { templates: Template[], dataEntries: DataEntry[] }) {
+    let startDate = new Date(this.dateService.dateLocalJSON(this.day.value.date).slice(0, 11) + this.shift.schedule.startTime);
+    let endDate = new Date(this.dateService.dateLocalJSON(this.day.value.date).slice(0, 11) + this.shift.schedule.endTime);
+    if (this.shift.part === 1) endDate = new Date(endDate.setDate(endDate.getDate() + 1))
+    if (this.shift.part === 2) startDate = new Date(startDate.setDate(endDate.getDate() - 1))
+    const templates = data.templates.map((template: Template) => {
+      let dataEntry: DataEntry = null;
+      // let svgIcon = this.svgIcon.missed;
+      data.dataEntries.map((item: DataEntry) => {
+        if ((item.template.templateId === template.templateId) && this.dateService.isBetween(item.createDate, startDate, endDate)) dataEntry = item
+      });
 
-    })
-  }
-  getDataEntries() {
-    this.store.pipe(
-      select(dataEntriesOnDate)
-    ).subscribe(dataEntries => {
-      this.dataEntries = dataEntries;
-    })
+      const getSvgIcon = () => {
+        if (new Date() < startDate) return this.svgIcon.lock;
+        if (!dataEntry) return this.svgIcon.missed;
+        if (dataEntry.submitDate) return this.svgIcon.submited;
+        return this.svgIcon.open;
+      }
+
+      return {
+        svgIcon: getSvgIcon(),
+        template,
+        dataEntry,
+      };
+    })  
+    this.templates = templates;
   }
 
-  svgSrc() {
-    return ''
+  /* 
+    getTemplates() {
+      this.templates$ = this.store.pipe(
+        select(allTemplates),
+        tap(list => this.templates = list)
+      ).subscribe()
+    }
+   
+    getDataEntries() {
+      this.dataEntries$ = this.store.pipe(
+        select(dataEntriesOnDate),
+        tap(dataEntries => this.dataEntries = dataEntries),
+      ).subscribe()
+    }
+   */
+  clickTemplate(template) {
+    console.log(template);
+
+    // const dataEntry: DataEntry = new DataEntry({
+    //   scheduleId: this.shift.schedule.scheduleId,
+    //   template: template,
+    // });
+    // this.store.dispatch(DataEntryActions.setCurrentDataEntry({ dataEntry }))
+    // this.router.navigate(['dataentry'])
   }
+
 }
