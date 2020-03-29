6b74e4f14ae3b5317b346e95754eba1d291c5316
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-03-29 23:43:11 +0300

    add week-grid add recur shifts, 1s update

--- src/app/modules/calendar/components/week-grid/week-grid.component.ts
+++ src/app/modules/calendar/components/week-grid/week-grid.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Schedule } from '@models/*';
 import { DateService } from 'src/app/services/date.service';
 
@@ -7,35 +7,60 @@ import { DateService } from 'src/app/services/date.service';
   templateUrl: './week-grid.component.html',
   styleUrls: ['./week-grid.component.scss']
 })
-export class WeekGridComponent implements OnChanges {
+export class WeekGridComponent implements OnInit, OnChanges, OnDestroy {
   @Input() schedules: Schedule[];
   @Input() week: number;
   @Input() year: number;
+
+  currentShiftStyle: string = " shadow-outline z-10";
 
   scheduleWeekList;
   startWeekDate = this.dateService.lastMonday();
   daysOfWeek = this.dateService.daysOfWeek;
   hours = new Array(24)
   shiftBg = [
-    // 'bg-gray-400',
-    'bg-red-400',
-    'bg-orange-400',
-    'bg-yellow-400',
-    'bg-green-400',
-    'bg-teal-400',
-    'bg-blue-400',
-    'bg-indigo-400',
-    'bg-purple-400',
-    'bg-pink-400',
+    // 'bg-gray-200',
+    'bg-red-200',
+    'bg-orange-200',
+    'bg-yellow-200',
+    'bg-green-200',
+    'bg-teal-200',
+    'bg-blue-200',
+    'bg-indigo-200',
+    'bg-purple-200',
+    'bg-pink-200',
   ]
-
+  interval;
   constructor(
     private dateService: DateService
   ) { }
 
+  ngOnInit() {
+    this.interval = setInterval(() => {
+      this.scheduleWeekList && Object.keys(this.scheduleWeekList).map(key => {
+        this.scheduleWeekList[key].shifts.map(shift => {
+          shift.elClass = shift.elClass.split(' ')[0] + this.getClass(this.scheduleWeekList[key], shift)
+        })
+      })
+    }, 1000)
+  }
+
   ngOnChanges(): void {
     this.startWeekDate = this.dateService.getMonday(this.year, this.week)
     this.scheduleWeekList = this.createScheduleWeekList(this.startWeekDate);
+  }
+  ngOnDestroy() {
+    clearInterval(this.interval);
+  }
+
+  getClass(day, shift) {
+    let result = '';
+    let startDate = new Date(this.dateService.dateLocalJSON(day.date).slice(0, 11) + shift.schedule.startTime);
+    let endDate = new Date(this.dateService.dateLocalJSON(day.date).slice(0, 11) + shift.schedule.endTime);
+    if (shift.part === 1) endDate = new Date(endDate.setDate(endDate.getDate() + 1));
+    if (shift.part === 2) startDate = new Date(startDate.setDate(endDate.getDate() - 1));
+    if (this.dateService.isBetween(new Date(), startDate, endDate)) result = result + " shadow-outline z-10"
+    return result;
   }
 
   createScheduleWeekList(startDay: Date) {
@@ -49,33 +74,47 @@ export class WeekGridComponent implements OnChanges {
         shifts: [],
       }
     });
+
     this.schedules.map((schedule: Schedule, index) => {
       const elClass = this.shiftBg[index]
-      this.dateService.daysOfWeek.map(({ key, dayNum, name }) => {
-        if (!schedule[key]) return;
+      this.dateService.daysOfWeek.map(({ key, dayNum }) => {
+        if (
+          !schedule[key]
+          || !this.dateService.isBetween(list[dayNum].date, schedule.validFromDate, schedule.validToDate)
+        ) return;
+        const weeks = this.dateService.getWeeks(list[dayNum].date, schedule.validFromDate);
         if (this.getMinutes(schedule.startTime) < this.getMinutes(schedule.endTime)) {
-          const shift = {
-            schedule,
-            position: this.getPos(schedule)[0],
-            elClass
-          };
-          list[dayNum].shifts.push(shift);
-        } else {
+          if (weeks % schedule.recurEveryWeeks) return;
           list[dayNum].shifts.push({
-            part: 1,
             schedule,
             position: this.getPos(schedule)[0],
-            elClass
+            elClass,
           });
-
-          list[dayNum === 7 ? 1 : dayNum + 1].shifts.push({
+          return;
+        }
+        if (weeks % schedule.recurEveryWeeks) {
+          if (((weeks - 1) % schedule.recurEveryWeeks) || dayNum === 7) return;
+          list[1].shifts.push({
             part: 2,
             schedule,
             position: this.getPos(schedule)[1],
             elClass
           });
-
-        }
+          return;
+        };
+        list[dayNum].shifts.push({
+          part: 1,
+          schedule,
+          position: this.getPos(schedule)[0],
+          elClass
+        });
+        if (dayNum === 7) return;
+        list[dayNum + 1].shifts.push({
+          part: 2,
+          schedule,
+          position: this.getPos(schedule)[1],
+          elClass
+        });
       })
     });
     return list
