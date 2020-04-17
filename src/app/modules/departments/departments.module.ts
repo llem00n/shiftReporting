c468acbceea6264a43f53463ce02141fdee5b1f7
PorubaimikhYehor <porubaimikh.yehor@gmail.com>
2020-04-17 10:40:21 +0300

    select User department

--- src/app/modules/departments/departments.module.ts
+++ src/app/modules/departments/departments.module.ts
@@ -1,18 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SelectUserDepartmentComponent } from './components/select-user-department/select-user-department.component';
-import { HttpClientModule } from '@angular/common/http';
-import { AngularSvgIconModule } from 'angular-svg-icon';
+// import { HttpClientModule } from '@angular/common/http';
+// import { AngularSvgIconModule } from 'angular-svg-icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { UsedMaterialModule } from '../used-material/used-material.module';
 
 
 
 @NgModule({
   declarations: [SelectUserDepartmentComponent],
   imports: [
-    AngularSvgIconModule.forRoot(),
+    // AngularSvgIconModule.forRoot(),
     CommonModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    UsedMaterialModule
   ],
   exports: [SelectUserDepartmentComponent]
 })
